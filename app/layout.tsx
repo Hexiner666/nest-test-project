24e696fb3aebb7d2b9959bf7a58eda790736ea75
Nucleus He <hexiner666@gmail.com>
2025-01-23 23:20:12 +0800

    [fix]:修复错误

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@
  * @Date: 2024-10-22 06:17:11
  */
 import '@/app/ui/global.css';
-import { inter } from 'import { lusitana } from '@/app/ui/fonts'
+import { inter } from '@/app/ui/fonts';
 
 export default function RootLayout({
   children,
